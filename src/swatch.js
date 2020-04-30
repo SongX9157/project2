b88bdd6ad500b2aa8dbacfd70a8d0eac34ded059
Song Xu <xus9157@uwec.edu>
2020-04-30 10:43:38 -0500

    1

---
+++ src/swatch.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function Swatch(){
+    return(
+        <div className="swatch">
+            <span className="hex">#000000</span>
+            <span className="delete-button">&#x2716;</span>
+            <span className="name">name</span>
+        </div>
+    );
+}
